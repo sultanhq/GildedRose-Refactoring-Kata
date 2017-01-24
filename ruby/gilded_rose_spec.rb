977abdef40d5f98232efcd2cf8bf6b0cb0c59464
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-24 14:43:54 +0000

    created passing test for reduce sellin value

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -8,6 +8,13 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+
+    it "reduces the sellin day value by one" do
+      items = [Item.new("foo", 10, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 9
+    end
+
   end
 
 end
