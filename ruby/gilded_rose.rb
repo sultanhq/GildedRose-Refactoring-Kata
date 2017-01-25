47d22958734ccc374617a02c47e5ec05b78c1ea3
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-25 12:17:31 +0000

    refactored increase/decrease into update quality methods

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -13,17 +13,11 @@ class GildedRose
           change_quality(item)
         end
       else
-        if item.quality < 50
-          increase_quality(item, 1)
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
             change_quality(item)
-          end
-        end
       end
       if item.name != "Sulfuras, Hand of Ragnaros"
         decrease_sell_in(item)
       end
-
       if item.sell_in < 0
         if item.name != "Backstage passes to a TAFKAL80ETC concert"
           if item.quality > 0
@@ -37,42 +31,29 @@ class GildedRose
   end
 end
 
-def decrease_quality(item,reduction)
-  item.quality = item.quality - reduction
-end
-
-def increase_quality(item,increament)
-  item.quality = item.quality + increament
-end
-
 def decrease_sell_in(item)
   item.sell_in = item.sell_in - 1
 end
 
 def change_quality(item)
   quality_matrix = {"foo" => -1, "Conjured" => -2, "Aged Brie" => +1, "Sulfuras, Hand of Ragnaros" => 0}
-  if item.name != "Backstage passes to a TAFKAL80ETC concert"
-    item.quality = item.quality + quality_matrix[item.name]
-  else
-    update_pass_quality(item)
+  if item.quality < 50
+    if item.name != "Backstage passes to a TAFKAL80ETC concert"
+      item.quality = item.quality + quality_matrix[item.name]
+    else
+      update_pass_quality(item)
+    end
   end
 end
 
 def update_pass_quality(item)
-  if item.sell_in < 11
-    if item.quality < 50
-      increase_quality(item, 1)
-    end
-  end
-  if item.sell_in < 6
-    if item.quality < 50
-      increase_quality(item, 1)
-    end
-  end
   if item.sell_in < 0
     item.quality = 0
+  elsif item.sell_in < 6
+    item.quality += 3
+  else
+    item.quality += 2
   end
-
 end
 
 class Item
