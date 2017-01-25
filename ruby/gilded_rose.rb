478176080f9cf5915293a4b10fb273e41df5f9e7
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-25 09:53:23 +0000

    passing test for Conjured items decreasing quality by twice as much

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -9,7 +9,11 @@ class GildedRose
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+            if item.name != "Conjured"
+              item.quality = item.quality - 1
+            else
+              item.quality = item.quality - 2
+            end
           end
         end
       else
