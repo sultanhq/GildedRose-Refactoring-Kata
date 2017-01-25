b9e7f0270e6741822e57ad4f97e30aa9fbed0325
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-25 09:40:02 +0000

    passed test for aged brie increasing in quality

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -27,6 +27,12 @@ describe GildedRose do
       expect(items[0].sell_in).to eq 9
     end
 
+    it "brie increases in quality value by one" do
+      items = [Item.new("Aged Brie", 10, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 11
+    end
+
   end
 
 end
