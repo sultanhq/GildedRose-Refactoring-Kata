cd31494e7dc416a1249ad6a55acf3060d9421047
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-25 09:50:49 +0000

    added new test for Conjured items

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -64,6 +64,12 @@ describe GildedRose do
       expect(items[0].quality).to eq 0
     end
 
+    it "Conjured items reduce the quality value by two" do
+      items = [Item.new("Conjured", 10, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 8
+    end
+
   end
 
 end
